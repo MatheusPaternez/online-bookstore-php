59b535907bae5169b0649979f2d181fb6e668797
Matheus Paternez <matheusgfpaternez2006@gmail.com>
2025-11-27 17:11:22 -0800

    Add form for get book data

--- main.php
+++ main.php
@@ -8,7 +8,23 @@
 </head>
 
 <body>
-    <form action=""></form>
+    <h1>Form Submission</h1>
+    <form method="post">
+        <!-- Title -->
+        <label for="title">Title:</label>
+        <input type="text" placeholder="Insert the title" id="title" name="title" /><br><br>
+        <!-- Author -->
+        <label for="author">Author:</label>
+        <input placeholder="Insert the author" id="author" type="text" name="author" /><br><br>
+        <!-- Genre -->
+        <label for="genre">Genre:</label>
+        <input placeholder="Insert the genre" id="genre" type="text" name="genre" /><br><br>
+        <!-- Price -->
+        <label for="price">Price:</label>
+        <input placeholder="Insert the price" id="price" type="number" name="price" /><br><br>
+        <!-- Submit -->
+        <input type="submit" value="Submit via POST">
+    </form>
 </body>
 
 </html>
